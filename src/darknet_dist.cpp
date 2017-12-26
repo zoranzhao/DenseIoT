97e2431db7c1064f2da5bffcf54f5b7511022abe
Zhuoran Zhao <zoranzhao@gmail.com>
2017-12-26 16:00:06 -0600

    Add local producer and local consumer thread

--- src/darknet_dist.cpp
+++ src/darknet_dist.cpp
@@ -94,21 +94,31 @@ void test_local(){
 
 }
 
-void load_image(network *net){
+void load_images(std::string thread_name){
+    network *net = parse_network_cfg("cfg/yolo.cfg");
     char filename[256];
     int id = 0;//5000 > id > 0
-    sprintf(filename, "data/val2017/%d.jpg", id);
-#ifdef NNPACK
-    image im = load_image_thread(filename, 0, 0, net->c, net->threadpool);
-    image sized = letterbox_image_thread(im, net->w, net->h, net->threadpool);
-#else
-    image im = load_image_color(filename,0,0);
-    image sized = letterbox_image(im, net->w, net->h);
-#endif
     unsigned int size;
-    size = (net->w)*(net->h)*(net->c);
-    put_job(sized.data, size, 0);
-    //ofs << "Thread "<< this_id <<" put task "<< id <<", size is: " << size << std::endl;   
+#ifdef DEBUG_DIST
+    std::ofstream ofs (thread_name + ".log", std::ofstream::out);
+#endif
+    for(id = 0; id < 100; id ++){
+         sprintf(filename, "data/val2017/%d.jpg", id);
+#ifdef NNPACK
+         image im = load_image_thread(filename, 0, 0, net->c, net->threadpool);
+         image sized = letterbox_image_thread(im, net->w, net->h, net->threadpool);
+#else
+         image im = load_image_color(filename, 0, 0);
+         image sized = letterbox_image(im, net->w, net->h);
+#endif
+         size = (net->w)*(net->h)*(net->c);
+         put_job(sized.data, size, 0);
+#ifdef DEBUG_DIST
+	 ofs << "Put task "<< id <<", size is: " << size << std::endl;  
+#endif 
+    }
+
+    //ofs << "Put task "<< id <<", size is: " << size << std::endl;   
     //std::cout << "Thread "<< this_id <<" put task "<< id <<", size is: " << size << std::endl; 
     //return im;  
 }
@@ -122,38 +132,30 @@ void get_image(image* im){
 }
 
 //"cfg/coco.data" "cfg/yolo.cfg" "yolo.weights" "data/dog.jpg"
-void test_detector_dist()
+void test_detector_dist(std::string thread_name)
 {
 
-    list *options = read_data_cfg("cfg/coco.data");
-    char *name_list = option_find_str(options, "names", "data/names.list");
-    char **names = get_labels(name_list);
-    float thresh = .24;
-    float hier_thresh = .5;
 
 
-    image **alphabet = load_alphabet();
+
+
+
     network *net = load_network("cfg/yolo.cfg", "yolo.weights", 0);
     set_batch_network(net, 1);
+
+    //load_image();
     srand(2222222);
-    int j;
-    float nms=.3;
 #ifdef NNPACK
     nnp_initialize();
     net->threadpool = pthreadpool_create(4);
 #endif
 
-
-#ifdef DEBUG_DIST
-    char filename[256];
+    int j;
     int id = 0;//5000 > id > 0
-    sprintf(filename, "data/val2017/%d.jpg", id);
-#endif
-    while(1){
-
+    for(id = 0; id < 5; id ++){
         image sized;
 	sized.w = net->w; sized.h = net->h; sized.c = net->c;
-	load_image(net);
+
         get_image(&sized);
         printf("Input image size is %d\n", sized.w*sized.h*sized.c);
         printf("Input image w is %d\n", sized.w);
@@ -165,6 +167,17 @@ void test_detector_dist()
 	network_predict_dist(net, X);
         double t2=what_time_is_it_now();
 #ifdef DEBUG_DIST
+	char filename[256];
+	char outfile[256];
+        float thresh = .24;
+        float hier_thresh = .5;
+	float nms=.3;
+        image **alphabet = load_alphabet();
+        list *options = read_data_cfg("cfg/coco.data");
+        char *name_list = option_find_str(options, "names", "data/names.list");
+        char **names = get_labels(name_list);
+	sprintf(filename, "data/val2017/%d.jpg", id);
+	sprintf(outfile, "%d", id);
         layer l = net->layers[net->n-1];
         float **masks = 0;
         if (l.coords > 4){
@@ -179,7 +192,7 @@ void test_detector_dist()
         get_region_boxes(l, im.w, im.h, net->w, net->h, thresh, probs, boxes, masks, 0, 0, hier_thresh, 1);
         if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
         draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, masks, names, alphabet, l.classes);
-        save_image(im, "predictions");
+        save_image(im, outfile);
         free(boxes);
         free_ptrs((void **)probs, l.w*l.h*l.n);
 #endif
@@ -200,7 +213,7 @@ void test_detector_dist()
 #endif
         }
 */
-	break;
+
     }
 #ifdef NNPACK
     pthreadpool_destroy(net->threadpool);
@@ -213,10 +226,11 @@ void test_detector_dist()
 
 int main(int argc, char **argv)
 {
+    std::thread local_producer(load_images, "local_producer");
+    std::thread local_consumer(test_detector_dist, "local_consumer");
 
-    //char *outfile = find_char_arg(argc, argv, "-out", 0);
-    //int fullscreen = find_arg(argc, argv, "-fullscreen");
-    test_detector_dist();
+    local_producer.join();
+    local_consumer.join();
 
     return 0;
 }
