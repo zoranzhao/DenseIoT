d13dadf9fa4a43dfa5273cabfa86c78f43fc86c6
Zhuoran Zhao <zoranzhao@gmail.com>
2017-12-29 17:36:54 -0600

    ...

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #cmd to update dependency repo
-cd ../darknet-nnpack && git pull && cd ../riot && git pull && cd ../yolo_dist
+git pull && cd ../darknet-nnpack && git pull && cd ../riot && git pull && cd ../yolo_dist
