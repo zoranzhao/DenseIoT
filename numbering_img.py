c9f0ec1e131f4bbe0922323ef96c31cf3ac21f25
Zhuoran Zhao <zoranzhao@gmail.com>
2017-12-30 16:33:21 -0600

    numbering image python script

---
+++ numbering_img.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import pprint
+import os
+
+from os import listdir
+from os.path import isfile, join
+mypath ="./data/val2017/"
+
+#onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+onlyimgs = [f for f in listdir(mypath) if ".jpg" in f ]
+
+#pprint.pprint(onlyfiles)
+#pprint.pprint(onlyimgs)
+
+for i in range(len(onlyimgs)):
+   os.rename(mypath+onlyimgs[i], mypath+(str(i)+".jpg"))
+
+# Load an color image in grayscale
+# img = cv2.imread('.jpg',0)
