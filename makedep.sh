44490897460ce6ddba64bc5ff193234987433990
Zhuoran Zhao <zoranzhao@gmail.com>
2018-01-01 16:56:35 -0600

    Force the lib linkage again

--- makedep.sh
+++ makedep.sh
@@ -1,2 +1,2 @@
 #Recompile the dependencies
-cd ../darknet-nnpack/ && make && cd ../riot && make && cd ../yolo_dist && make
+cd ../darknet-nnpack/ && make && cd ../riot && make && cd ../yolo_dist && touch obj/* && make
