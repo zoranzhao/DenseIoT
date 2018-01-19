981865469075e6c0eea989dc1b5e7e0d06f995e6
Zhuoran Zhao <zoranzhao@gmail.com>
2018-01-18 19:35:16 -0600

    Changing the name of Directory

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #cmd to update dependency repo
-git pull && cd ../darknet-nnpack && git pull && cd ../riot && git pull && cd ../yolo_dist
+git pull && cd ../darknet-nnpack && git pull && cd ../riot && git pull && cd ../DenseIoT
