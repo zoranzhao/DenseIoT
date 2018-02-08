c6b9e3c2f0a36c1f668fa530c7c059df4a93c0d1
Zhuoran Zhao <zoranzhao@gmail.com>
2018-02-07 21:20:20 -0600

    ...

--- src/darknet_dist.cpp
+++ src/darknet_dist.cpp
@@ -4,7 +4,8 @@
 
 int main(int argc, char **argv)
 {
-    victim_client();
+    idle_client();
+    //victim_client();
     //smart_gateway();
     //client_register_gateway();
     //client_steal_gateway();
