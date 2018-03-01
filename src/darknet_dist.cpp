f7962a1f7ad4e7a73ad9e402636f636792a16d09
Zhuoran Zhao <zoranzhao@gmail.com>
2018-03-01 11:39:22 -0600

    ...

--- src/darknet_dist.cpp
+++ src/darknet_dist.cpp
@@ -7,10 +7,10 @@ int main(int argc, char **argv)
     //victim_client_local();
 
     //victim_client_shuffle();
-    idle_client_shuffle();
+    //idle_client_shuffle();
     //smart_gateway_shuffle();
 
-    //idle_client();
+    idle_client();
     //victim_client();
     //smart_gateway();
 
