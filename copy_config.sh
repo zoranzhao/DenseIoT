0769cd332ea668e3ec5dc13e312afdd42fef9225
Zhuoran Zhao <zoranzhao@gmail.com>
2018-03-08 11:36:05 -0600

    ...

--- copy_config.sh
+++ copy_config.sh
@@ -1,6 +1,6 @@
-scp ./include/config.h 192.168.4.9:/home/zzhao/DenseIoT/include
-scp ./include/config.h 192.168.4.8:/home/zzhao/DenseIoT/include
-scp ./include/config.h 192.168.4.4:/home/zzhao/DenseIoT/include
-scp ./include/config.h 192.168.4.14:/home/zzhao/DenseIoT/include
-scp ./include/config.h 192.168.4.15:/home/zzhao/DenseIoT/include
-scp ./include/config.h 192.168.4.16:/home/zzhao/DenseIoT/include
+scp ./config.sh 192.168.4.9:/home/zzhao/DenseIoT
+scp ./config.sh 192.168.4.8:/home/zzhao/DenseIoT
+scp ./config.sh 192.168.4.4:/home/zzhao/DenseIoT
+scp ./config.sh 192.168.4.14:/home/zzhao/DenseIoT
+scp ./config.sh 192.168.4.15:/home/zzhao/DenseIoT
+scp ./config.sh 192.168.4.16:/home/zzhao/DenseIoT
